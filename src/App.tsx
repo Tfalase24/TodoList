80990a30ebc236ad3153049acb6bbd183ff4a0da
TF <tfalase24@gmail.com>
2023-02-20 17:48:33 +0000

    feat(App.tsx): added useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import InputField from "./components/InputField";
 
 let App: React.FC = () => {
+  let [todo, setTodo] = useState("");
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 };
