c034c628463ccf45cad498acb6f828fa8571fb63
TF <tfalase24@gmail.com>
2023-02-21 11:01:46 +0000

    feat(src\App.tsx): useState, handleAdd

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 let App: React.FC = () => {
   let [todo, setTodo] = useState("");
+  let [todos, setTodos] = useState<Todo[]>([]);
+
+  let handleAdd = () => {};
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 };
